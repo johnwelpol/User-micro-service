a08b86682fe19ac9e3eaed5e8ede12efc735e694
jpol <jpol@yondu.com>
2019-11-19 11:07:01 +0800

    update database configuration structure

--- config/database.php
+++ config/database.php
@@ -1,22 +1,36 @@
 <?php
 
 return [
-    'mysql_only_connect'  => [
-        'driver'    => 'mysql',
-        'host'      => env('DB_HOST', '127.0.0.1'),
-        'port'      => env('DB_PORT', '3306'),
-        'database'  => null,
-        'username'  => env('DB_USERNAME', 'root'),
-        'password'  => env('DB_PASSWORD', '')
-    ],
+    'migrations' => 'mysql',
+    'default' => 'mysql',
 
-    'mysql'  => [
-        'driver'    => 'mysql',
-        'host'      => env('DB_HOST', '127.0.0.1'),
-        'port'      => env('DB_PORT', '3306'),
-        'database'  => "User_micro_service",
-        'username'  => env('DB_USERNAME', 'root'),
-        'password'  => env('DB_PASSWORD', '')
-    ],
+    'connections' => [
+        'mysql_only_connect'  => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST'),
+            'port'      => env('DB_PORT'),
+            'database'  => null,
+            'username'  => env('DB_USERNAME'),
+            'password'  => env('DB_PASSWORD')
+        ],
 
+        'mysql'  => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST', '127.0.0.1'),
+            'port'      => env('DB_PORT', '3306'),
+            'database'  => env('DB_DATABASE'),
+            'username'  => env('DB_USERNAME', 'root'),
+            'password'  => env('DB_PASSWORD', '')
+        ],
+
+        'INFORMATION_SCHEMA' =>
+        [
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST', '127.0.0.1'),
+            'port'      => env('DB_PORT', '3306'),
+            'database'  => 'INFORMATION_SCHEMA',
+            'username'  => env('DB_USERNAME', 'root'),
+            'password'  => env('DB_PASSWORD', '')
+        ],
+    ]
 ];
