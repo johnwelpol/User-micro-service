6afec021c5fb2bfbae053e87f0779b835162759a
Peter Joshua Brion <pbrion@yondu.com>
2019-11-19 16:27:00 +0800

    Add Ramsey/UUID dependency and Fix set database connection.

--- app/DbConnection.php
+++ app/DbConnection.php
@@ -1,7 +1,11 @@
 <?php
 
+namespace App;
+
 use Illuminate\Database\Eloquent\Model;
 
-class DbConfigurations extends Model{
-
+class DbConnection extends Model{
+    protected $table = "db_configurations";
+    protected $guarded = [];
+    public $timestamps = false;
 }
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,18 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\DbConnection;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Schema;
 use Laravel\Lumen\Routing\Controller as BaseController;
+
 
 class Controller extends BaseController
 {
 
+    public function __construct()
+    {
+        
+    }
+
     public function setDatabaseConnection(Request $request){
-        // validation code here ...
-        $this->validate($request, [
+
+        // validation code for DB credentials
+        $database_credentials = $this->validate($request, [
             'driver' => 'required|string',
             'host' => 'required|string',
             'port' => 'required|string',
@@ -21,24 +31,26 @@ class Controller extends BaseController
             'username' => 'required|string',
         ]);
 
+        if ($request->name) {
+            Config::set("database.connections.". $request->name, $database_credentials);
+        }
+        // dd(DbConnection::fin);
+            
         // initialize database schemma
-        DB::connection('mysql_only_connect')->statement("CREATE DATABASE testing");
-        
-        // model code here ..
-        DbConnection::insert($request);
-
-        // create token when finish the settings of the database connection
+        if (!Schema::hasTable($request->database)) {
+            DB::connection('mysql_only_connect')->statement("CREATE DATABASE $request->database"); 
+        }
         $token = Hash::make(Str::uuid());
-
+        $request->request->add(['token'=>$token]);
+        DbConnection::create($request->except(['name', 'id']));
+    
         // create response parser here ...
-        
-        return ;
+        return response()->json(['message'=>'Success.', 'code' => 200]);
     }
 
     public function createMigrationScript(){
-
+        $migrator = app('migrator');    
     }
-
 
     public function runMigration(){
 
