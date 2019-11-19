5e3ffee52b6d83be16d9e531627691d4d800a422
jpol <jpol@yondu.com>
2019-11-19 11:08:18 +0800

    set logical framework

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
 
-    public function index(){
-        return "hi";
+    public function setDatabaseConnection(Request $request){
+        // validation code here ...
+        $this->validate($request, [
+            'driver' => 'required|string',
+            'host' => 'required|string',
+            'port' => 'required|string',
+            'database' => ['required', 'string', 'unique:db_configurations'],
+            'username' => 'required|string',
+        ]);
+
+        // initialize database schemma
+        DB::connection('mysql_only_connect')->statement("CREATE DATABASE testing");
+        
+        // model code here ..
+        DbConnection::insert($request);
+
+        // create token when finish the settings of the database connection
+        $token = Hash::make(Str::uuid());
+
+        // create response parser here ...
+        
+        return ;
+    }
+
+    public function createMigrationScript(){
+
+    }
+
+
+    public function runMigration(){
+
     }
 }
